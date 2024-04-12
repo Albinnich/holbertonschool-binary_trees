b56afc8e6998a16aba61cb23e7f0053a2f9e73b8
Albinnich <albina_haji@hotmail.com>
2024-04-12 15:20:15 +0000

    Else statement added

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,18 +13,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_height;
 
 	if (tree == NULL)
+	{
 		return (0);
-
+	}
+	else
+	{
 	left_height = binary_tree_height(tree->left);
 
 	right_height = binary_tree_height(tree->right);
 
-	if (left_height > right_height)
-	{
-		return (left_height + 1);
-	}
-	else
-	{
-		return (right_height + 1);
+	return (1 + (left_height > right_height ? left_height : right_height));
 	}
 }
