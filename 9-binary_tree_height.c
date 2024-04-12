4476f27450bb3ab23aa0e62a0eedce9f8154199b
Albinnich <albina_haji@hotmail.com>
2024-04-12 15:26:27 +0000

    Return statement changed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height;
 	size_t right_height;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 	{
 		return (0);
 	}
