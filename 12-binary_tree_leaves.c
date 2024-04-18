7821fb784201a8d8104940ab51fb9502d7a1d487
Albinnich <albina_haji@hotmail.com>
2024-04-18 12:16:09 +0000

    Code added

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,6 +12,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	else if
+		(tree->left == NULL && tree->right == NULL)
+			return (1);
 	else
-		return (1 + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
