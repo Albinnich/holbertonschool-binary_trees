16fe455a1e82c94f85a0f556dacd81a4f953059c
Albinnich <albina_haji@hotmail.com>
2024-04-18 10:28:59 +0000

    Initial try

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - function to measure depth of node
+ * @tree: pointer to node
+ * Return: always 0
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t left_depth;
+	size_t right_depth;
+
+	else
+	{
+		left_depth = binary_tree_depth(tree->left);
+
+		right_depth = binary_tree_height(tree->right);
+
+		return (1 + (left_depth > right_depth ? left_depth : right_depth));
+	}
+}
