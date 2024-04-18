5a1bc20cd85a1618b6014fa9498defcbc022c58d
Albinnich <albina_haji@hotmail.com>
2024-04-18 11:08:00 +0000

    Location of variables changed

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,14 +9,14 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 
 {
-	size_t left_depth;
-	size_t right_depth;
-
 	if (tree == NULL)
 		return (0);
 
 	else
 	{
+		size_t left_depth;
+		size_t right_depth;
+
 		left_depth = binary_tree_depth(tree->left);
 		right_depth = binary_tree_depth(tree->right);
 
